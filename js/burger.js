cce224c70961a1007c875b870dc19796aa678865
Greyson <rs.greysonz@gmail.com>
2018-12-19 08:11:31 -0600

    Update burger.js

--- js/burger.js
+++ js/burger.js
@@ -2,9 +2,24 @@ $(document).ready(function() {
 
     // Check for click events on the navbar burger icon
     $(".navbar-burger").click(function() {
-  
+    
         // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
         $(".navbar-burger").toggleClass("is-active");
         $(".navbar-menu").toggleClass("is-active");
     });
-  });
+
+    $(document).click(function(event) {
+        $(".navbar-burger").removeClass("is-active");
+        $(".navbar-menu").removeClass("is-active");
+    })
+});
+
+$(document).click(function(event) { 
+        if(!$(event.target).closest('.navbar').length) {
+            
+            if($('.navbar-menu').hasClass("is-active")) {
+                $('.navbar-burger').removeClass('is-active');
+                $('.navbar-menu').removeClass('is-active');
+            }
+        }        
+    });
